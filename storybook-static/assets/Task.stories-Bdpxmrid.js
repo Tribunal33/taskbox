ab8beb93c0177ca2b92902231c4a80ab62903eb9
Tribunal33 <Tribunal33@users.noreply.github.com>
2024-10-24 09:25:36 -0700

    add build directory

---
+++ storybook-static/assets/Task.stories-Bdpxmrid.js
@@ -0,0 +1,23 @@
+import{f as e}from"./index-DLz49pOH.js";import{T as u}from"./Task-B0zBud2q.js";import"./vue.esm-bundler-BDMoQPGI.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const T={onPinTask:e(),onArchiveTask:e()},S={component:u,title:"Task",tags:["autodocs"],excludeStories:/.*Data$/,args:{...T}},s={args:{task:{id:"1",title:"Test Task",state:"TASK_INBOX"}}},a={args:{task:{...s.args.task,state:"TASK_PINNED"}}},t={args:{task:{...s.args.task,state:"TASK_ARCHIVED"}}};var r,o,n;s.parameters={...s.parameters,docs:{...(r=s.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    task: {
+      id: '1',
+      title: 'Test Task',
+      state: 'TASK_INBOX'
+    }
+  }
+}`,...(n=(o=s.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,i,d;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_PINNED'
+    }
+  }
+}`,...(d=(i=a.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var m,p,k;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_ARCHIVED'
+    }
+  }
+}`,...(k=(p=t.parameters)==null?void 0:p.docs)==null?void 0:k.source}}};const f=["ActionsData","Default","Pinned","Archived"];export{T as ActionsData,t as Archived,s as Default,a as Pinned,f as __namedExportsOrder,S as default};
